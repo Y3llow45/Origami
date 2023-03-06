0a572ecdd6136895128e585c2de5a9fa25effc40
Yellow <bofengejn@gmail.com>
2023-03-06 21:09:50 +0200

    2

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,18 @@ function App() {
       <h1>Let's begin</h1>
       <nav className={style.navigation}>
         <ul>
-          <li className="listItem"><a href="#">Going to 1</a></li>
-          <li className="listItem"><a href="#">Going to 2</a></li>
-          <li className="listItem"><a href="#">Going to 3</a></li>
-          <li className="listItem"><a href="#">Going to 4</a></li>
-          <li className="listItem"><a href="#">Going to 5</a></li>
-          <li className="listItem"><a href="#">Going to 6</a></li>
-          <li className="listItem"><a href="#">Going to 7</a></li>
-          <li className="listItem"><a href="#">Going to 8</a></li>
-          <li className="listItem"><a href="#">Going to 9</a></li>
-          <li className="listItem"><a href="#">Going to 10</a></li>
-          <li className="listItem"><a href="#">Going to 11</a></li>
+          <li className="listItem"><img src="/white-origami-bird.png" alt="white origami" /></li>
+          <li className="listItem"><a href="https://github.com/">Going to 1</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 2</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 3</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 4</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 5</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 6</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 7</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 8</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 9</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 10</a></li>
+          <li className="listItem"><a href="https://github.com/">Going to 11</a></li>
         </ul>
       </nav>
     </div>
