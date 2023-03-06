9d19e3368a3518ddceea3a9b5ba917c01d2b4c77
Yellow <bofengejn@gmail.com>
2023-03-06 18:42:28 +0200

    1

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,21 @@ function App() {
   return (
     <div className={style.app}>
       <h1>Let's begin</h1>
+      <nav className={style.navigation}>
+        <ul>
+          <li className="listItem"><a href="#">Going to 1</a></li>
+          <li className="listItem"><a href="#">Going to 2</a></li>
+          <li className="listItem"><a href="#">Going to 3</a></li>
+          <li className="listItem"><a href="#">Going to 4</a></li>
+          <li className="listItem"><a href="#">Going to 5</a></li>
+          <li className="listItem"><a href="#">Going to 6</a></li>
+          <li className="listItem"><a href="#">Going to 7</a></li>
+          <li className="listItem"><a href="#">Going to 8</a></li>
+          <li className="listItem"><a href="#">Going to 9</a></li>
+          <li className="listItem"><a href="#">Going to 10</a></li>
+          <li className="listItem"><a href="#">Going to 11</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
